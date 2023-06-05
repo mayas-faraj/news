5e9f6c3614add669c91e2497683d56d8305a6180
uniqueprogrammer <uniqueprogrammer@hotmail.com>
2023-06-05 15:21:14 +0300

    create taxonomies, messages, settings

---
+++ backend/src/api/taxonomy/controllers/taxonomy.ts
@@ -0,0 +1,7 @@
+/**
+ * taxonomy controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::taxonomy.taxonomy');
---
+++ backend/src/api/taxonomy/routes/taxonomy.ts
@@ -0,0 +1,7 @@
+/**
+ * taxonomy router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::taxonomy.taxonomy');
